f593f993387060bb1fb00a7b4d46a47d34fdf613
DarkMatt3r06 <darkmatt3r06@gmail.com>
2024-03-03 16:35:49 +0100

    Project Initiation

---
+++ CCC.py
@@ -0,0 +1,135 @@
+import pytube
+import os
+import zipfile
+
+
+
+def remove_illegalCharacters_fromString ( string ):
+    # remove illegal characters from the string
+    
+    illegalCharacters = ["/" , "\\" , ":" , "*" , "?" , "\"" , "<" , ">" , "|"]
+    
+    for character in illegalCharacters:
+        string = string.replace(character, "")
+    
+    return string
+
+
+
+def instance_ytObject ( url ):
+    # instance the object yt from the URL
+    
+    if "watch" in url:
+        yt = pytube.YouTube(url)
+        return yt
+    elif "playlist" in url:
+        yt = pytube.Playlist(url)
+        return yt
+    elif "channel" in url:
+        yt = pytube.Channel(url)
+        return yt
+    
+    return None
+
+
+
+def download ( yt , pathOf_download , zipFile ):
+    # download the video, playlist or channel
+    
+    # download of one video
+    if isinstance(yt, pytube.YouTube):
+        yt.title = remove_illegalCharacters_fromString(yt.title)
+        
+        yt.streams.get_highest_resolution().download( output_path=pathOf_download , filename=(yt.title+".mp4") )
+        
+        return
+    
+    # download of a playlist or channel
+    for video in yt.videos:
+        video.title = remove_illegalCharacters_fromString(video.title)
+        
+        video.streams.get_highest_resolution().download( output_path=pathOf_download , filename=(video.title+".mp4") )
+        
+        # add the video to the zip file and remove the video from output_path
+        zipFile.write(video.title+".mp4")
+        os.remove(video.title+".mp4")
+    
+    zipFile.close()
+        
+                
+
+def main ():
+    
+    print("Welcome to Cancel Cancel Culture!")
+    print("Read the README file for instructions on how to use this program.")
+    
+    print("")
+    
+    
+    
+    #. input of URL
+    while True:
+        url = input("Enter the URL of the video, playlist or channel you want to download: ")
+
+        try:
+            yt = instance_ytObject(url)
+            if yt != None:
+                break
+            
+            print("Invalid URL. Please try again.")
+        except:
+            print("Invalid URL. Please try again.")
+    
+    
+    
+    print("")
+    
+    
+        
+    #. input destination folder
+    while True:
+        pathOf_download = input("Enter the destination folder: ")
+        
+        if os.path.exists(pathOf_download):
+            break
+        
+        print("Invalid folder. Please try again.")
+    os.chdir(pathOf_download)
+    
+    
+    
+    print("")
+
+
+    
+    #. input of zip file name
+    nameOf_zipFile = None
+    while True:
+        nameOf_zipFile = input("Enter the name of the zip file: ")
+        
+        if nameOf_zipFile != "":
+            
+            if ".zip" not in nameOf_zipFile:
+                nameOf_zipFile += ".zip"
+            
+            zipFile = zipfile.ZipFile(nameOf_zipFile, "w")
+            
+            break
+        
+        print("Invalid name. Please try again.")
+        
+        
+        
+    #. download
+    download( yt , pathOf_download , zipFile )
+    
+     
+            
+    print("")
+            
+    print("Thank you for using Cancel Cancel Culture!")
+    
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,50 @@
+# CCC
+CCC stands for *Cancel Cancel Culture*. This tool allows users to download single youtube videos, playlists and videos from a specific channel, saving them locally to prevent them from being cancelled.
+
+
+
+## Table of Contents
+
+- [Warning/Disclaimer](#warningdisclaimer)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Author](#author)
+
+
+
+## Warning/Disclaimer
+
+This tool is not intended for piracy or any other illegal activities. It is intended to be used to download videos that are at risk of being deleted or censored. The user is responsible for the use of this tool and the videos downloaded with it.
+
+
+
+## Getting Started
+
+CCC requires the presence on running system of pytube, a lightweight, dependency-free Python library that allows you to download YouTube videos. 
+
+To install pytube, run the following command in your terminal:
+```bash
+pip install pytube
+```
+
+
+
+## Usage
+
+To use CCC run the script as you'd do normally, and stick to the following instructions:
+
+- When prompted, enter the URL of the video, playlist or channel you want to download. 
+    *Note that if you want to download a playlist or a channel you have to follow the following format: `https://www.youtube.com/playlist?list=...` or `https://www.youtube.com/channel/UC...`*.
+
+- Choose the download path for the video, playlist or channel you want to download.
+    *Note that the format of the path must be `C:\Users\user\Downloads\` or `/home/user/Downloads/`*. 
+
+- Choose the preferred video quality. If the video does not have the selected quality, the script will download the best available quality.
+
+- If you want to download a playlist or a channel you have to choose the name of the zip file that will contain all the videos.
+
+
+
+# Author
+
+[DarkMatt3r06](https://github.com/DarkMatt3r06)
